1af8cb0a62d6224d425b0efd09d41476df71e1a1
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2023-10-20 12:23:02 +0300

    56.Gün Çalışmaları Eklendi.

---
+++ Day 56/script.js
@@ -0,0 +1,76 @@
+const Person = function(firstName, birthYear){
+    this.firstName = firstName
+    this.birthYear = birthYear
+    this.calcAge = function(){
+        console.log(2023 - this.birthYear)
+    }
+}
+
+const berkcan = new Person('Berkcan', 1999)
+console.log(berkcan)
+berkcan.calcAge()
+
+const matilda = new Person('Matilda', 2017)
+const jack = new Person('Jack', 1975)
+console.log(matilda, jack)
+jack.calcAge()
+
+console.log(berkcan instanceof Person)
+
+console.log(Person.prototype)
+Person.prototype.calcAge = function(){
+    console.log(2023 - this.birthYear)
+}
+
+berkcan.calcAge()
+console.log(berkcan.__proto__) 
+console.log(berkcan.__proto__ === Person.prototype) // true
+console.log(Person.prototype.isPrototypeOf(berkcan)) // true 
+
+Person.prototype.species = "Homo Sapiens"
+console.log(berkcan.species, matilda.species)
+
+console.log(berkcan.hasOwnProperty("firstName"))
+console.log(matilda.hasOwnProperty("species"))
+
+
+console.log(berkcan.__proto__)
+console.log(berkcan.__proto__.__proto__)
+
+// Object.prototype 
+
+console.dir(Person.prototype.constructor)   // Person constructorını verir.
+
+const arr = [3,6,4,5,6,9]
+console.log(arr.__proto__)
+console.log(arr.__proto__ === Array.prototype) // true
+console.log(arr.__proto__.__proto__)
+
+Array.prototype.unique = function() {
+    return [...new Set(this)]
+}
+
+console.log(arr.unique())
+
+
+const Car = function (make, speed){
+    this.make = make
+    this.speed = speed
+}
+
+Car.prototype.accelerate = function (){
+    this.speed += 10
+    console.log(`${this.make} : ${this.speed} km\sa`)
+}
+
+Car.prototype.brake = function (){
+    this.speed -= 5
+    console.log(`${this.make} : ${this.speed} km\sa`)
+}
+
+const bmv = new Car("BMW",120)
+const mercedes = new Car("Mercedes", 95)
+
+bmv.accelerate()
+mercedes.accelerate()
+
